4d12c03bdbae3bd73df5c4f8ea15fc7ef494b81d
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2021-11-26 19:54:42 +0200

    Update build.sh

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -96,7 +96,7 @@ if [[ $LTO = "true" ]]; then
 	CFLAGS="$CFLAGS -flto=thin"
 	CXXFLAGS="$CXXFLAGS -flto=thin"
 	# emulated-tls should not be needed in ndk r18 https://github.com/android-ndk/ndk/issues/498#issuecomment-327825754
-	LDFLAGS="$LDFLAGS -flto=thin -Wl,-z,notext,-plugin-opt=-emulated-tls -fuse-ld=lld"
+	LDFLAGS="$LDFLAGS -flto=thin -Wl,-plugin-opt=-emulated-tls -fuse-ld=lld"
 fi
 
 if [[ $ARCH = "arm" ]]; then
