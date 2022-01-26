af258b2b7aa4ae623c8fcf4f574132f2c6e097f1
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2022-01-26 16:58:34 +0200

    Update build.sh

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -93,8 +93,8 @@ else
 fi
 
 if [[ $LTO = "true" ]]; then
-	CFLAGS="$CFLAGS -flto -ffast-math"
-	CXXFLAGS="$CXXFLAGS -flto -ffast-math"
+	CFLAGS="$CFLAGS -flto"
+	CXXFLAGS="$CXXFLAGS -flto"
 	# emulated-tls should not be needed in ndk r18 https://github.com/android-ndk/ndk/issues/498#issuecomment-327825754
 	LDFLAGS="$LDFLAGS -flto -Wl,-plugin-opt=-emulated-tls -fuse-ld=gold"
 fi
