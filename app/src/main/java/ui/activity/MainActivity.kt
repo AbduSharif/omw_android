abc72021de97214e47b1707f2dec35b1449eaeee
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2022-02-08 13:23:13 +0200

    Update MainActivity.kt

--- app/src/main/java/ui/activity/MainActivity.kt
+++ app/src/main/java/ui/activity/MainActivity.kt
@@ -428,8 +428,6 @@ class MainActivity : AppCompatActivity() {
                 // openmw.cfg: data, resources
                 file.Writer.write(Constants.OPENMW_CFG, "resources", Constants.RESOURCES)
                 file.Writer.write(Constants.OPENMW_CFG, "data", "\"" + inst.findDataFiles() + "\"")
-                file.Writer.write(Constants.OPENMW_CFG, "user-data=?userdata?")
-                file.Writer.write(Constants.OPENMW_CFG, "config=?userconfig?")
                 file.Writer.write(Constants.OPENMW_CFG, "encoding", prefs!!.getString("pref_encoding", GameInstaller.DEFAULT_CHARSET_PREF)!!)
 
                 configureDefaultsBin(mapOf(
