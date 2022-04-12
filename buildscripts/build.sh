3b829a233183b78e6a8fba687fb68dcbe7c3ba81
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2022-04-12 23:43:48 +0200

    Update build.sh

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -131,7 +131,13 @@ NCPU=$(grep -c ^processor /proc/cpuinfo)
 echo "==> Build using $NCPU CPUs"
 mkdir -p build/$ARCH/
 mkdir -p prefix/$ARCH/
-cp ./icu-release-70-1/icu4c/source/ prefix/$ARCH/icu-host-build/
+mkdir -p prefix/$ARCH/icu-host-build/
+cd prefix/$ARCH/icu-host-build/
+wget https://github.com/unicode-org/icu/archive/refs/tags/release-70-1.zip
+unzip release-70-1.zip
+ICU_SOURCE_DIR=./icu-release-70-1/icu4c/source
+${ICU_SOURCE_DIR}/configure --disable-tests --disable-samples --disable-icuio --disable-extras
+make -j $(nproc)
 
 # symlink lib64 -> lib so we don't get half the libs in one directory half in another
 mkdir -p prefix/$ARCH/lib
