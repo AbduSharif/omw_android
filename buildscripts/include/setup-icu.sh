eddc6ac659306407dada79f9b3420d50a7f905a3
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2022-04-19 04:46:46 +0200

    Create setup-icu.sh

---
+++ buildscripts/include/setup-icu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p ${prefi}/icu-host-build/
+cd ${prefix}/icu-host-build/
+wget https://github.com/unicode-org/icu/archive/refs/tags/release-70-1.zip
+unzip release-70-1.zip
+ICU_SOURCE_DIR=${prefix}/icu-host-build/icu-release-70-1/icu4c/source
+${ICU_SOURCE_DIR}/configure --disable-tests --disable-samples --disable-icuio --disable-extras
+make -j $(nproc)
+cd ..
