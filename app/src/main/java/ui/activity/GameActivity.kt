823b56276b566ee465637f3e51a85e9e3f3b9b83
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2023-03-17 01:11:07 +0200

    Update GameActivity.kt

--- app/src/main/java/ui/activity/GameActivity.kt
+++ app/src/main/java/ui/activity/GameActivity.kt
@@ -109,6 +109,7 @@ class GameActivity : SDLActivity() {
         }
 
         System.loadLibrary("GL")
+        System.loadLibrary("libcollada-dom2.5-dp")
         System.loadLibrary("openmw")
     }
 
