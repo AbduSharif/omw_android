6d1b69d3116d888bcef11058d65591109ffa7a5a
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2023-05-06 16:33:05 +0200

    Update version.sh

--- buildscripts/include/version.sh
+++ buildscripts/include/version.sh
@@ -6,7 +6,7 @@ ANDROID_API="21"
 
 # End of configurable options
 
-NDK_FILE="ndk-$NDK_VERSION.zip"
+NDK_FILE="android-ndk-10078971-linux-x86_64.zip"
 
 if [[ $ARCH = "arm" ]]; then
 	ABI="armeabi-v7a"
