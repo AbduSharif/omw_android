002b410357c1082331904e8b9b20efb7045a5e41
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2023-05-06 16:00:13 +0200

    Update download-ndk.sh

--- buildscripts/include/download-ndk.sh
+++ buildscripts/include/download-ndk.sh
@@ -13,7 +13,7 @@ if [[ -f $NDK_FILE ]]; then
 	exit 0
 fi
 
-curl --http1.1 "https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux.zip" > $NDK_FILE
+curl --http1.1 "https://ci.android.com/builds/submitted/10078971/linux/latest/android-ndk-10078971-linux-x86_64.zip" > $NDK_FILE
 
 echo "==> Checking NDK zip file integrity"
 FILE_HASH=$(sha256sum $NDK_FILE | awk '{print $1}' )
