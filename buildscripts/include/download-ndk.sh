44a15206f84c86b8b1c7b1daeb327fffa3623785
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2023-05-06 18:11:23 +0200

    Update download-ndk.sh

--- buildscripts/include/download-ndk.sh
+++ buildscripts/include/download-ndk.sh
@@ -13,9 +13,6 @@ if [[ -f $NDK_FILE ]]; then
 	exit 0
 fi
 
-curl --http1.1 "https://www.mediafire.com/file/y5dr1bj869b8622/android-ndk-10078971-linux-x86_64.zip?dkey=x3ya3qvgyx6&r=649" > $NDK_FILE
-
-echo "==> Checking NDK zip file integrity"
-FILE_HASH=$(sha256sum $NDK_FILE | awk '{print $1}' )
+curl --http1.1 "https://www.mediafire.com/file/y5dr1bj869b8622/android-ndk-10078971-linux-x86_64.zip" > $NDK_FILE
 
 popd
