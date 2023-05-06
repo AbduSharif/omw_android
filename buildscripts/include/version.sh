a9e408bebd708b3cbf85aab5485cb7a7141edcc5
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2023-05-06 16:07:45 +0200

    Update version.sh

--- buildscripts/include/version.sh
+++ buildscripts/include/version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #NDK_VERSION="437b7b75b9ba1ecb825e37c8514e28def7ceb7bc"
-NDK_HASH="ab9dbab873fff677deb2cfd95ea60b9295ebd53b58ec8533e9e1110b2451e540"
+NDK_HASH="36a115bcb125c2d1fd6eb5f763eb9ef913a95004f8c084e9c92a5f5e0b4ea589"
 ANDROID_API="21"
 
 # End of configurable options
