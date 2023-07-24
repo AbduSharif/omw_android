443be838d4abdba2152dff92d1960edbc83930a3
AbduSharif <57298920+AbduSharif@users.noreply.github.com>
2023-07-24 08:35:44 +0200

    Update build.sh

--- buildscripts/build.sh
+++ buildscripts/build.sh
@@ -12,7 +12,7 @@ LTO="false"
 BUILD_TYPE="release"
 CFLAGS="-fPIC"
 CXXFLAGS="-fPIC -frtti -fexceptions"
-LDFLAGS="-fPIC --no-undefined-version=true"
+LDFLAGS="-fPIC --no-undefined-version=false"
 
 usage() {
 	echo "Usage: ./build.sh [--help] [--asan] [--arch arch] [--debug|--release]"
